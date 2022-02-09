b20c25f7cba14fff7dd9727e57ca66aa0386396a
Laura Salas <laurasalas96@live.com>
2022-02-08 21:52:11 -0500

    Update evil.js

--- evil.js
+++ evil.js
@@ -2,8 +2,8 @@ var markup = document.documentElement.innerHTML;
 window.onload = function() {
     var count_styles_a = (markup.match(/style=/g) || []).length;
     count_styles_b = (markup.match(/<style>/g) || []).length;
-    if (count_styles_a + count_styles_b > 2) {
-        document.body.parentNode.innerHTML = "<p> ATTENTION, tu n'as PAS le droit de mettre tes propres definitions de style pour ce devoir, enleve-les et n'utilise seulement que les attributs pre-definis (did you think you could fool me? MOUAHAHA!!!)</p>";
+    if (count_styles_a + count_styles_b > 0) {
+        document.body.parentNode.innerHTML = "<p> ATTENTION, tu n'as PAS le droit de mettre tes propres definitions de style / CSS pour ce devoir, enleve-les et n'utilise seulement que les attributs pre-definis (did you think you could fool me? MOUAHAHA!!!)</p>";
     }
     
 }
