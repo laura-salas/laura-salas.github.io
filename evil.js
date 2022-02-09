65c6eac28bc49d8fb7d085cad00397276e3837dd
Laura Salas <laurasalas96@live.com>
2022-02-08 22:10:06 -0500

    Update evil.js

--- evil.js
+++ evil.js
@@ -4,7 +4,10 @@ window.onload = function() {
     var count_styles_b = (markup.match(/<style>/g) || []).length;
     var count_styles_c = (markup.match(/rel="stylesheet"/g) || []).length;
     var count_styles_d = (markup.match(/github\.io\/krustyStyleS/g) || []).length; 
-    if (count_styles_a + count_styles_b > 0 || count_styles_c > 1 || count_styles_d < 1) {
+    if (count_styles_d < 1) {
+        document.body.parentNode.innerHTML = "<p> ATTENTION, tu as enlevé le CSS obligatoire du devoir (celui dans le <head> de l'enoncé), s'il te plaît remets-le avant de continuer!";
+    }
+    else if (count_styles_a + count_styles_b > 0 || count_styles_c > 1) {
         document.body.parentNode.innerHTML = "<p> ATTENTION, tu n'as PAS le droit de mettre tes propres definitions de style / CSS pour ce devoir, enleve-les et n'utilise seulement que les attributs pre-definis (did you think you could fool me? MOUAHAHA!!!)</p>";
     }
 
