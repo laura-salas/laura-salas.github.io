4a228b403f1c50b9e04a7c0c3c95642d17da9449
Laura Salas <laurasalas96@live.com>
2022-02-08 21:41:57 -0500

    Add files via upload

---
+++ evil.js
@@ -0,0 +1,9 @@
+var markup = document.documentElement.innerHTML;
+window.onload = function() {
+    var count_styles_a = (markup.match(/style=/g) || []).length;
+    count_styles_b = (markup.match(/<style>/g) || []).length;
+    if (count_styles_a + count_styles_b > 2) {
+        alert("ATTENTION, tu n'as PAS le droit de mettre tes propres definitions de style pour ce devoir, enleve-les et n'utilise seulement que les attributs pre-definis (did you think you could fool me? MOUAHAHA!!!)");
+    }
+    document.body.parentNode.innerHTML = "<p> enleve tes ajouts de style du code html pour pouvoir continuer :-)</p>";
+}
